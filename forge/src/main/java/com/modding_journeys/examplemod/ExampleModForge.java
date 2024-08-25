2ee4a9aaf7f7e86ec704642cf3f319ae32cebf4e
jason13official <135896906+jason13official@users.noreply.github.com>
2024-08-24 23:14:21 -0400

    Update ExampleModForge.java

--- forge/src/main/java/com/modding_journeys/examplemod/ExampleModForge.java
+++ forge/src/main/java/com/modding_journeys/examplemod/ExampleModForge.java
@@ -1,6 +1,9 @@
 package com.modding_journeys.examplemod;
 
+import com.modding_journeys.ender_pearl_staff.core.registry.ModRegistryForge;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(Constants.MOD_ID)
 public class ExampleModForge {
